9fcaf4cdd067971e6da33f8425fc046b93d7c3bd
KarishmaBothara <bothara.karishma@gmail.com>
2021-08-20 13:56:06 +1200

    Attempt 6 to fix test

--- jest.config.js
+++ jest.config.js
@@ -4,18 +4,26 @@
 const config = require('@polkadot/dev/config/jest');
 const findPackages = require('./scripts/findPackages');
 
+const internalModules = findPackages().reduce((modules, { dir, name }) => {
+  modules[`${name}(.*)$`] = `<rootDir>/packages/${dir}/src/$1`;
+
+  return modules;
+}, {});
+
+
 module.exports = {
   ...config,
   moduleNameMapper: {
-    ...(
-      findPackages()
-        .filter(({ name }) => !['@polkadot/apps'].includes(name))
-        .reduce((modules, { dir, name }) => {
-          modules[`${name}(.*)$`] = `<rootDir>/packages/${dir}/src/$1`;
-
-          return modules;
-        }, {})
-    ),
+    // ...(
+    //   findPackages()
+    //     .filter(({ name }) => !['@polkadot/apps'].includes(name))
+    //     .reduce((modules, { dir, name }) => {
+    //       modules[`${name}(.*)$`] = `<rootDir>/packages/${dir}/src/$1`;
+    //
+    //       return modules;
+    //     }, {})
+    // ),
+    ...internalModules,
     '@polkadot/apps/(.*)$': '<rootDir>/packages/apps/src/$1',
     '\\.(css|less)$': 'empty/object',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'empty/object',
